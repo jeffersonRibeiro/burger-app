83819797aa258e31ed917f99b9f5c52f002cb742
jefferson Ribeiro <jefferson.r.silva@infracommerce.com.br>
2018-04-19 22:12:57 -0300

    Burger ingredients

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BurgerBuilder extends Component {
+  render() {
+    return(
+      <React.Fragment>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default BurgerBuilder;
