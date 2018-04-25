ffe1ca0750b11ebf44ad68cdf075fd1e5c4ace64
JeffersonRibeiro <jefferson.ribeiro.contato@gmail.com>
2018-04-24 21:04:02 -0300

    Lesson 118 - done

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
           return [...Array(props.ingredients[igKey])].map( (_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />
           });
-        });
+        })
+        .reduce( (arr, el) => arr.concat(el), []);
   
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
   
   return(
     <div className={classes.Burger}>
