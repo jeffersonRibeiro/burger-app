01f8f68e291fb94e2f6aca985b438e622a90fa1f
jefferson Ribeiro <jefferson.r.silva@infracommerce.com.br>
2018-04-27 14:25:12 -0300

    Lesson 131 - Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
   <React.Fragment>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>
       {props.children}
     </main>
