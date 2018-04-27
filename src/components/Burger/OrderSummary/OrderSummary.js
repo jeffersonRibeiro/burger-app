af492e088d1d094e05281f13152883dbb0c0ccf3
jefferson Ribeiro <jefferson.r.silva@infracommerce.com.br>
2018-04-27 14:00:00 -0300

    Lesson 130 - Done

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -15,6 +15,7 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
       <p>Continue to checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
       <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
