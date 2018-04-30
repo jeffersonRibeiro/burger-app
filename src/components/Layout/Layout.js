26a7bb10e54bd39ea216f85c035950557c2de946
jefferson Ribeiro <jefferson.r.silva@infracommerce.com.br>
2018-04-30 15:39:49 -0300

    Lesson 137 - Done

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <React.Fragment>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </React.Fragment>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  }
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
 
-export default layout;
+  render() {
+    return(
+      <React.Fragment>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+          />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,18 +3,25 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../../Navigation/NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
-  // ...
+  let attachedClasses = [classes.SideDrawer, classes.Close]
+  if(props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
   return(
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <React.Fragment>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </React.Fragment>
   );
 };
 
