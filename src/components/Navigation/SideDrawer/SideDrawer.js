00ae58c98ae002feeebfbd9a5bdfe4ffc163463b
jefferson Ribeiro <jefferson.r.silva@infracommerce.com.br>
2018-04-30 12:40:01 -0300

    Lesson 134 - SideDrawer

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../../Navigation/NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+
+const sideDrawer = (props) => {
+  // ...
+  return(
+    <div className={classes.SideDrawer}>
+      <Logo />
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  );
+};
+
+export default sideDrawer;
