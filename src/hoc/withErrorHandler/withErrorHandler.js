d791dcbd99388ae1afa214761ec0ec034be1342b
jefferson Ribeiro <jefferson.r.silva@infracommerce.com.br>
2018-05-09 17:38:32 -0300

    Lesson 172 - Remoing Old Interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,14 +8,19 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null,
     }
     componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.reInterceptor = axios.interceptors.request.use(req => {
         this.setState({error: null});
         return req;
       });
 
-      axios.interceptors.response.use(res => res, error => {
+      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
         this.setState({error});
       });
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmedHanler = () => {
