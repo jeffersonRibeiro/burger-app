35dee36649910e0e24c8ee0e641906e0e5314a19
JeffersonRibeiro <jefferson.ribeiro.contato@gmail.com>
2018-05-12 09:08:44 -0300

    Lesson 207 - Building the checkout page

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
+class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      chesse: 1,
+      bacon: 1,
+    }
+  }
+  render() {
+    return (
+      <CheckoutSummary
+        ingredients={this.state.ingredients}
+      />
+    );
+  }
+}
+
+export default Checkout;
